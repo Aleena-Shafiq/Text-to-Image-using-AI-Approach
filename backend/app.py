3f35127eda9f31c1c156963175677b280e51e212
Aleena-Shafiq <96982026+Aleena-Shafiq@users.noreply.github.com>
2023-06-27 11:54:11 +0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -7,30 +7,33 @@ import time
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
-from stable_diffusion_wrapper import StableDiffusionWrapper
+
 from consts import DEFAULT_IMG_OUTPUT_DIR, MAX_FILE_NAME_LEN
-from utils import parse_arg_boolean
+from utils import parse_arg_boolean, parse_arg_dalle_version
+from consts import ModelSize
 
 app = Flask(__name__)
 CORS(app)
 print("--> Starting the image generation server. This might take up to two minutes.")
 
-stable_diff_model = None
+from dalle_model import DalleModel
+dalle_model = None
 
-parser = argparse.ArgumentParser(description = "A text-to-image app to turn your textual prompts into visionary delights")
+parser = argparse.ArgumentParser(description = "A DALL-E app to turn your textual prompts into visionary delights")
 parser.add_argument("--port", type=int, default=8000, help = "backend port")
+parser.add_argument("--model_version", type = parse_arg_dalle_version, default = ModelSize.MINI, help = "Mini, Mega, or Mega_full")
 parser.add_argument("--save_to_disk", type = parse_arg_boolean, default = False, help = "Should save generated images to disk")
 parser.add_argument("--img_format", type = str.lower, default = "JPEG", help = "Generated images format", choices=['jpeg', 'png'])
 parser.add_argument("--output_dir", type = str, default = DEFAULT_IMG_OUTPUT_DIR, help = "Customer directory for generated images")
 args = parser.parse_args()
 
-@app.route("/generate", methods=["POST"])
+@app.route("/dalle", methods=["POST"])
 @cross_origin()
 def generate_images_api():
     json_data = request.get_json(force=True)
     text_prompt = json_data["text"]
     num_images = json_data["num_images"]
-    generated_imgs = stable_diff_model.generate_images(text_prompt, num_images)
+generated_imgs = dalle_model.generate_images(text_prompt, num_images)
 
     returned_generated_images = []
     if args.save_to_disk:
@@ -60,8 +63,10 @@ def health_check():
 
 
 with app.app_context():
-    stable_diff_model = StableDiffusionWrapper()
-    print("--> Image generation server is up and running!")
+  dalle_model = DalleModel(args.model_version)
+    dalle_model.generate_images("warm-up", 1)
+    print("--> DALL-E Server is up and running!")
+    print(f"--> Model selected - DALL-E {args.model_version}")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=args.port, debug=False)
