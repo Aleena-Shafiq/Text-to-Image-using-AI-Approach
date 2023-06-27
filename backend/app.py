c5e0dd80d697fa5858f08973f7231478bbb0620b
Aleena-Shafiq <96982026+Aleena-Shafiq@users.noreply.github.com>
2023-06-27 13:00:12 +0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,13 @@
-import argparse
-import base64
-import os
-from pathlib import Path
-from io import BytesIO
-import time
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
-
-from consts import DEFAULT_IMG_OUTPUT_DIR, MAX_FILE_NAME_LEN
+from consts import DEFAULT_IMG_OUTPUT_DIR
 from utils import parse_arg_boolean, parse_arg_dalle_version
 from consts import ModelSize
 
 app = Flask(__name__)
 CORS(app)
-print("--> Starting the image generation server. This might take up to two minutes.")
+print("--> Starting DALL-E Server. This might take up to two minutes.")
 
 from dalle_model import DalleModel
 dalle_model = None
@@ -33,41 +26,21 @@ def generate_images_api():
     json_data = request.get_json(force=True)
     text_prompt = json_data["text"]
     num_images = json_data["num_images"]
-generated_imgs = dalle_model.generate_images(text_prompt, num_images)
+    generated_imgs = dalle_model.generate_images(text_prompt, num_images)
 
-returned_generated_images = []
-if args.save_to_disk:
-    dir_name = os.path.join(args.output_dir,f"{time.strftime('%Y-%m-%d_%H-%M-%S')}_{text_prompt}")[:MAX_FILE_NAME_LEN]
-    Path(dir_name).mkdir(parents=True, exist_ok=True)
-
-for idx, img in enumerate(generated_imgs):
+    returned_generated_images = []
     if args.save_to_disk: 
-        img.save(os.path.join(dir_name, f'{idx}.{args.img_format}'), format=args.img_format)
-
-    buffered = BytesIO()
-    img.save(buffered, format=args.img_format)
-    img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
-    returned_generated_images.append(img_str)
-
-print(f"Created {num_images} images from text prompt [{text_prompt}]")
-
-response = {
-    'generatedImgs': returned_generated_images,
-    'generatedImgsFormat': args.img_format
-}
-return jsonify(response)
-
-@app.route("/", methods=["GET"])
-@cross_origin()
-def health_check():
-    return jsonify(success=True)
+Expand Down
+Expand Up
+	@@ -62,11 +60,9 @@ def health_check():
 
 
 with app.app_context():
-  dalle_model = DalleModel(args.model_version)
+    dalle_model = DalleModel(args.model_version)
     dalle_model.generate_images("warm-up", 1)
     print("--> DALL-E Server is up and running!")
     print(f"--> Model selected - DALL-E {args.model_version}")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=args.port, debug=False)
