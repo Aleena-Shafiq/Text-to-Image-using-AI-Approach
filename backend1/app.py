02eaa7abc7dcfaf484e1016535d24b846205c2d3
Aleena-Shafiq <96982026+Aleena-Shafiq@users.noreply.github.com>
2023-06-27 16:24:04 +0500

    Update app.py

--- backend1/app.py
+++ backend1/app.py
@@ -4,6 +4,7 @@ import os
 from pathlib import Path
 from io import BytesIO
 import time
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from consts import DEFAULT_IMG_OUTPUT_DIR
@@ -35,9 +36,25 @@ def generate_images_api():
 
     returned_generated_images = []
     if args.save_to_disk: 
-Expand Down
-Expand Up
-	@@ -62,11 +60,9 @@ def health_check():
+        dir_name = os.path.join(args.output_dir,f"{time.strftime('%Y-%m-%d_%H-%M-%S')}_{text_prompt}")
+        Path(dir_name).mkdir(parents=True, exist_ok=True)
+    
+    for idx, img in enumerate(generated_imgs):
+        if args.save_to_disk: 
+          img.save(os.path.join(dir_name, f'{idx}.{args.img_format}'), format=args.img_format)
+        buffered = BytesIO()
+        img.save(buffered, format=args.img_format)
+        img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+        returned_generated_images.append(img_str)
+    print(f"Created {num_images} images from text prompt [{text_prompt}]")
+    
+    response = {'generatedImgs': returned_generated_images,
+    'generatedImgsFormat': args.img_format}
+    return jsonify(response)
+@app.route("/", methods=["GET"])
+@cross_origin()
+def health_check():
+    return jsonify(success=True)
 
 
 with app.app_context():
