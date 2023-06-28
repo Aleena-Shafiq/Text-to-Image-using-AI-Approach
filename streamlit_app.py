c06771076786f1d3278e31f7cba3ba6ae286d7d6
Aleena-Shafiq <96982026+Aleena-Shafiq@users.noreply.github.com>
2023-06-28 15:57:22 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import requests
 import base64
 import streamlit as st
+from PIL import Image
+from io import BytesIO
 
 headers = {'Bypass-Tunnel-Reminder': "go", 'mode': 'no-cors'}
 
@@ -28,5 +30,29 @@ def create_and_show_images(url, text, num_images):
             generatedImgs = data['generatedImgs']
             for index in range(len(generatedImgs)):
                 img = base64.b64decode(generatedImgs[index])
-                image_tag = f'<img src="data:image/png;base64,{base64.b64encode(img).decode()}" style="border-radius: 10px; margin: 10px;">'
-                st.markdown(image_tag, unsafe_allow_html=True)
+                image = Image.open(BytesIO(img))
+
+                # Apply border-radius style to the image
+                image_style = f"border-radius: 10px;"
+                st.image(image, use_column_width=True, output_format="PNG", style=image_style)
+
+                # Add download link
+                download_link = f'<a href="data:image/png;base64,{base64.b64encode(img).decode()}" download="image{index}.png">Download Image</a>'
+                st.markdown(download_link, unsafe_allow_html=True)
+
+st.markdown("<h1 style='text-align: center;'>Text to Image converter </h1>", unsafe_allow_html=True)
+
+# Insert your name
+st.markdown("<div align='center'><i>Created By: Aleena Shafiq (20-CP-90)</i></div>", unsafe_allow_html=True)
+st.markdown("<div align='center'><i>Created By: Muhammad Umer (20-CP-78)</i></div>", unsafe_allow_html=True)
+
+url = st.text_input("Enter the backend URL")
+
+text = st.text_input("What should I create?")
+
+num_images = st.slider("How many images?", 1, 6)
+
+ok = st.button("GO!")
+
+if ok:
+    create_and_show_images(url, text, num_images)
